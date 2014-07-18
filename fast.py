adecdf7468595bdff24bacbc7e90654f7eb283da
Fahad Almusharraf <ifahad7@gmail.com>
2014-07-18 16:55:13 -0400

    update

--- fast.py
+++ fast.py
@@ -12,10 +12,17 @@ if __name__ == "__main__":
   while(1):
     ret, frame = capture.read()
 
+
     kp = fast.detect(frame, None)
     img = cv2.drawKeypoints(frame, kp, color=(255,0,0))
 
     cv2.imshow('FAST', img)
+
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    kp_gray = fast.detect(gray_image, None)
+    img_gray = cv2.drawKeypoints(gray_image, kp_gray, color=(255,0,0))
+
+    cv2.imshow('FAST GRAY', img_gray)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
       img = frame
